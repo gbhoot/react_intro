9e1eed6fd2f3995a60ec4716eb56af1ed7790deb
gbhoot <gsbhoot1020@gnail.com>
2019-04-08 21:49:11 -0700

    added text input +Greeting component with message

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greetings from './Greetings';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+class SimpleForm extends React.Component {
+	render () {
+		return (
+			<div>
+				<input type="text" name="firstName" />
+				<Greetings firstName="John" />
+    	</div>
     );
-  }
+	}
 }
+  
+const App = () => (
+	<div>
+    <SimpleForm />
+	</div>
+);
+
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//       >Learn React</a>
+//     </header>
+//   </div>
+// )
 
 export default App;
